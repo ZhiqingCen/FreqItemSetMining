adaac2710403558fc16e54d1512da1d2142ca7ff
Zhiqing <zhiqingcen.zc@gmail.com>
2023-11-07 16:59:32 +1100

    project2 - submission

--- project2.df.py
+++ project2.df.py
@@ -1,14 +1,70 @@
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+from itertools import combinations
+from pyspark.sql import Window
 import sys
 
-class Project2:           
+class Project2:
+    def __init__(self):
+        self.invoiceNo = "invoiceNo"        # the unique ID to record one purchase transaction
+        self.description = "description"    # the name of the item in a transaction
+        self.quantity = "quantity"          # the amount of the items purchased
+        self.invoiceDate = "invoiceDate"    # the time of the transaction
+        self.unitPrice = "unitPrice"        # the price of a single item
+
     def run(self, inputPath, outputPath, k):
         spark = SparkSession.builder.master("local").appName("project2_df").getOrCreate()
-        
+        spark.conf.set("spark.sql.legacy.timeParserPolicy", "LEGACY")
         # Fill in your code here
-       
+        
+        # define schema of dataframe
+        schema = StructType([\
+                    StructField(self.invoiceNo, IntegerType(), True),\
+                    StructField(self.description, StringType(), True),\
+                    StructField(self.quantity, IntegerType(), True),\
+                    StructField(self.invoiceDate, StringType(), True),\
+                    StructField(self.unitPrice, FloatType(), True)])
+        
+        # get data from input file
+        fileDF = spark.read.format("csv").option("header", "false").schema(schema).load(inputPath)
+        dataDF = fileDF.select(self.invoiceNo, self.description, self.invoiceDate)
+
+        # format invoiceDate
+        dataDF = dataDF.withColumn(self.invoiceDate, to_timestamp(self.invoiceDate, "dd/M/yyyy h:mm:ss a"))
+        dataDF = dataDF.withColumn(self.invoiceDate, date_format(self.invoiceDate, "M/yyyy"))
+        
+        # get the list of itemSets and count monthlyInvoices for each (invoiceDate, invoiceNo) pairs
+        monthlyInvoicesDF = dataDF.groupBy(self.invoiceDate, self.invoiceNo).agg(collect_set(self.description).alias("itemSets"))
+        monthlyInvoicesDF = monthlyInvoicesDF.groupBy(self.invoiceDate).agg(count(self.invoiceNo).alias("monthlyInvoices"), collect_set("itemSets").alias("itemSets"))
+        monthlyInvoicesDF = monthlyInvoicesDF.withColumn("itemSets", explode("itemSets"))
+
+        # get itemSets containing 3 items
+        itemSetsUdf = udf(lambda i : list(combinations(i, 3)), ArrayType(ArrayType(StringType())) )
+        itemSetsDF = monthlyInvoicesDF.withColumn("itemSets", explode(itemSetsUdf("itemSets")))
+        
+        # sort description in itemSets and format itemSets to match output
+        sortItemSetsUdf = udf(lambda i : f"({'|'.join(sorted(i))})")
+        itemSetsDF = itemSetsDF.withColumn("itemSets", sortItemSetsUdf("itemSets"))
+
+        # calculate support value
+        monthlyItemSetsDF = itemSetsDF.groupBy(self.invoiceDate, "itemSets", "monthlyInvoices").agg(count("itemSets").alias("monthlyItemSets"))
+        supportDF = monthlyItemSetsDF.withColumn("support", col("monthlyItemSets")/col("monthlyInvoices"))
+        supportDF = supportDF.select(self.invoiceDate, "itemSets", "support")
+
+        # get topK and sort output
+        window = Window.orderBy(desc("support"), "itemSets").partitionBy(self.invoiceDate)
+        topKDF = supportDF.withColumn("topK", row_number().over(window))
+        topKDF = topKDF.filter(col("topK") <= int(k)).select(self.invoiceDate, "itemSets", "support")
+
+        # format date and sort
+        topKDF = topKDF.withColumn(self.invoiceDate, to_timestamp(self.invoiceDate, "M/yyyy")).orderBy(self.invoiceDate)
+        topKDF = topKDF.withColumn(self.invoiceDate, date_format(self.invoiceDate, "M/yyyy"))
+        
+        # output to file
+        topKDF.coalesce(1)
+        topKDF.write.csv(outputPath)
+        
         spark.stop()
 
 if __name__ == "__main__":
--- project2_rdd.py
+++ project2_rdd.py
@@ -1,14 +1,60 @@
 from pyspark import SparkContext, SparkConf
+from itertools import combinations
+from operator import add
+from datetime import datetime
+import heapq
 import sys
 
-class Project2:   
-        
+class Project2:
+    def __init__(self):
+        self.invoiceNo = 0      # the unique ID to record one purchase transaction
+        self.description = 1    # the name of the item in a transaction
+        self.quantity = 2       # the amount of the items purchased
+        self.invoiceDate = 3    # the time of the transaction
+        self.unitPrice = 4      # the price of a single item
+
     def run(self, inputPath, outputPath, k):
         conf = SparkConf().setAppName("project2_rdd").setMaster("local")
         sc = SparkContext(conf=conf)
-        
         # Fill in your code here
+        # detect the top-k frequent item sets from the log for each month
+        
+        # load data from file
+        file = sc.textFile(inputPath)
+        lines = file.map(lambda line : line.split(","))
+        
+        # keep only invoiceNo, description and formatted invoiceDate data
+        data = lines.map(lambda i : (i[self.invoiceNo], (set(i[self.description]), i[self.invoiceDate].split(" ", 1)[0].split("/", 1)[1])))
 
+        # group items according to each invoiceNo
+        invoices = data.reduceByKey(lambda i, j : (i[self.invoiceNo] | j[0], i[1]))
+        invoices = invoices.map(lambda i : (i[self.invoiceNo], (sorted(i[1][0]), i[1][1])))
+
+        # get item sets containing 3 items
+        threeItems = invoices.map(lambda i : (i[0], (list(combinations(i[1][0], 3)), i[1][1])))
+
+        # map according to item sets
+        itemSets = threeItems.flatMap(lambda i : [(i[0], ("|".join(items), i[1][1])) for items in i[1][0]])
+        invoicesByNo = itemSets.map(lambda i : ((i[0], i[1][1]), 1)).groupByKey().mapValues(len)
+        
+        # count number of unique invoices for each month
+        monthlyInvoices = invoices.map(lambda i : (i[0], i[1][1])).map(lambda i : (i[1], 1)).groupByKey().mapValues(len)
+        
+        # count number of unique itemSets for each month
+        monthlyItemSets = itemSets.map(lambda i : ((i[1][1], i[1][0]), 1)).groupByKey().mapValues(len).map(lambda i : (i[0][0], (i[0][1], i[1])))
+
+        # calculate support value
+        # convert to output format = MONTH/YEAR,(Item1|Item2|Item3), support value
+        support = monthlyItemSets.join(monthlyInvoices).map(lambda i : (i[0], f"({i[1][0][0]})", i[1][0][1] / i[1][1]))
+        
+        # sort output and get topK rows only
+        monthlySortSupport = support.groupBy(lambda i : i[0])
+        topK = monthlySortSupport.mapValues(lambda i : heapq.nsmallest(int(k), i, key=lambda j : (datetime.strptime(j[0], "%m/%Y"), -j[2], j[1])))
+        topK = topK.flatMap(lambda i : i[1]).map(lambda i : (i[0], i[1], str(i[2])))#.map(lambda i : ",".join(i)).coalesce(1)
+        topK = topK.sortBy(lambda i : datetime.strptime(i[0], "%m/%Y")).map(lambda i : ",".join(i)).coalesce(1)
+        
+        # output to file
+        topK.saveAsTextFile(outputPath)
         sc.stop()
 
 if __name__ == "__main__":
@@ -16,4 +62,3 @@ if __name__ == "__main__":
         print("Wrong arguments")
         sys.exit(-1)
     Project2().run(sys.argv[1], sys.argv[2], sys.argv[3])
-
